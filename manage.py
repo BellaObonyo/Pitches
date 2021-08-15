ba6ea2ba7e12e8d95d4236f9d1a4f714b5391001
ObonyoCynthia <cynthiabella.obonyo@gmail.com>
2021-08-15 19:45:20 +0300

    updated config.py file

--- manage.py
+++ manage.py
@@ -1,18 +1,26 @@
-from app import create_app
-from flask_script import Manager,Server
+from flask_script import Manager, Server
+from flask_migrate import Migrate, MigrateCommand
+from app import create_app,db
+from app.models import User
 
-# Creating app instance
-app = create_app('development')
+app = create_app('production')
 
 manager = Manager(app)
 manager.add_command('server',Server)
 
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app = app,db = db,User = User)
+
 @manager.command
 def test():
-    """Run the unit tests."""
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     manager.run()
--- tests/user_test.py
+++ tests/user_test.py
@@ -6,3 +6,13 @@ from app.models import User
 class UserTest(unittest.TestCase):
     def setUp(self):
         self.new_user = User(username='Obonyo',password='44444')
+
+    def test_password_setter(self):
+        self.assertTrue(self.new_user.password is not None)
+
+    def test_no_access_password(self):
+        with self.assertRaises(AttributeError):
+            self.new_user.password
+
+    def test_password_verification(self):
+        self.assertTrue(self.new_user.verify_password('44444'))
