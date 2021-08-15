72fa34996e8650f533fc8eb9d42282d201c6121f
ObonyoCynthia <cynthiabella.obonyo@gmail.com>
2021-08-15 18:17:18 +0300

    updated  forms.py

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,13 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SelectField, TextAreaField,SubmitField
+from wtforms import StringField, SelectField, TextAreaField, SubmitField
 from wtforms.validators import Required
 
-
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Write a brief bio about you your.',validators = [Required()])
+    bio = TextAreaField('Write something brief about you.',validators = [Required()])
     submit = SubmitField('Save')
 
+class PitchForm(FlaskForm):
+    title = StringField('Title', validators=[Required()])
+    category = SelectField('Category', choices=[('Technology','Technology'),('Business','Business'),('Programming','Programming'),('Religion','Religion'),('Sports','Sports'),('Social','Social')],validators=[Required()])
+    post = TextAreaField('Your Pitch', validators=[Required()])
+    submit = SubmitField('Pitch')
 
-    class PitchForm(FlaskForm):
-        title = StringField('Title', validators=[Reuired()])
-        category = SelectField('Category',choices=['Technology','Technology'])
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Leave a comment',validators=[Required()])
+    submit = SubmitField('Comment')
