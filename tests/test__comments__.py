c8ee4ee938478ed53677461c18b3430fcd92e200
ObonyoCynthia <cynthiabella.obonyo@gmail.com>
2021-08-15 19:24:22 +0300

    updated comments.py file

--- tests/test__comments__.py
+++ tests/test__comments__.py
@@ -3,7 +3,7 @@ from app import db
 
 def setUp(self):
   self.user_Bella= User(username = 'Cynthia',password = '123456', email = 'obonyocy@ueab.ac.ke')
-  self.new_comment = Comment(id=1,user_id=10,comment='This Pitch is nice', pitch_id = 12)
+  self.new_comment = Comment(id=1,user_id=10,comment='This Pitch is nice', pitch_id = 9)
 
 def tearDown(self):
   Comment.query.delete()
@@ -12,7 +12,7 @@ def tearDown(self):
     self.assertEquals(self.new_comment.id,1)
     self.assertEquals(self.new_comment.user_id,10)
     self.assertEquals(self.new_comment.comment,'This Pitch is nice')
-    self.assertEquals(self.new_comment.pitch_id,12)
+    self.assertEquals(self.new_comment.pitch_id,9)
         
 def test_save_comment(self):
   self.new_comment.save_comment()
